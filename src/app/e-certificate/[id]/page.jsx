5deb9bb1cdf76125289a9bf8c2197a7eadebcb42
indra Mahesa <indramahesa128@gmail.com>
2024-08-23 13:38:27 +0800

    fix: maps not showing & detail

--- src/app/e-certificate/[id]/page.jsx
+++ src/app/e-certificate/[id]/page.jsx
@@ -1,14 +1,6 @@
 import { makeTerraxActor } from "@/core/services/actorLocatorService";
 import CertificateGenerateView from "@/features/certificate/views/CertificateGenerateView";
 
-export function generateStaticParams() {
-  return [
-    {
-      id: "1",
-    },
-  ];
-}
-
 const Page = () => {
   return <CertificateGenerateView />;
 };
--- src/app/properties/[id]/page.jsx
+++ src/app/properties/[id]/page.jsx
@@ -1,14 +1,6 @@
 import { makeTerraxActor } from "@/core/services/actorLocatorService";
 import PropertyListView from "@/features/properties/views/PropertyListView";
 
-export function generateStaticParams() {
-  return [
-    {
-      id: "1",
-    },
-  ];
-}
-
 function Home() {
   return <PropertyListView />;
 }
